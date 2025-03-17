1798d92ee309fe3228e186c21f9c781ad87453c1
aangelo-21 <martinezglielmo@gmail.com>
2025-03-17 16:41:04 +0000

    Added every label and text field, also added them to the panel.

---
+++ Formulario.java
@@ -0,0 +1,69 @@
+// Importamos las utilidades
+
+import javax.swing.*;
+
+public class Formulario extends JFrame {
+
+  //Creamos un main, que llame al metodo de iniciar componentes
+  public Formulario() {
+    initComponents();
+  }
+
+  //Esta es la ventana que se va a mostrar
+  public void initComponents() {
+
+    //Añadimos las caracteristicas bases de la ventana
+    setSize(500, 700);
+    setTitle("Formulario");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Añadimos los componentes
+
+    //Componentes de nombre
+    JLabel etiquetaNombre = new JLabel("Introduce tu nombre");
+    JTextField textoNombre = new JTextField(25);
+
+    //Componentes de apellidos
+    JLabel etiquetaApellidos = new JLabel("Introduce tus apellidos");
+    JTextField textoApellidos = new JTextField(25);
+
+    //Componentes de DNI
+    JLabel etiquetaDNI = new JLabel("Introduce tu DNI");
+    JTextField textoDNI = new JTextField(25);
+
+    //Componentes de telefono
+    JLabel etiquetaTelefono = new JLabel("Introduce tu telefono");
+    JTextField textoTelefono = new JTextField(25);
+
+    //Componentes de correo electronico
+    JLabel etiquetaCorreo = new JLabel("Introduce tu correo electronico");
+    JTextField textoCorreo = new JTextField(25);
+
+    //Componentes de contraseña
+    JLabel etiquetaPassword = new JLabel("Introduce tu contraseña");
+    JTextField textoPassword = new JTextField(25);
+
+    JPanel panel = new JPanel();
+
+    //Añadimos todo a la ventana
+    panel.add(etiquetaNombre);
+    panel.add(textoNombre);
+    panel.add(etiquetaApellidos);
+    panel.add(textoApellidos);
+    panel.add(etiquetaDNI);
+    panel.add(textoDNI);
+    panel.add(etiquetaTelefono);
+    panel.add(textoTelefono);
+    panel.add(etiquetaCorreo);
+    panel.add(textoCorreo);
+    panel.add(etiquetaPassword);
+    panel.add(textoPassword);
+    setContentPane(panel);
+    setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    new Formulario();
+  }
+
+}
